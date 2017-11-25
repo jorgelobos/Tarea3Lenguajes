a265104b33bc4b56a5c928a40c230ea17d58265c
Jorge Lobos <jorge.lobos@ug.uchile.cl>
2017-11-24 21:42:42 -0300

    Agregados comentarios

--- tarea3.py
+++ tarea3.py
@@ -3,16 +3,21 @@ import sys
 
 
 def parser(string):
-    """ Rexp Environment -> Value Environment
-            Recursively evaluates the given Rexp in the given environment"""
+    """ String -> RExpr
+        Parse a RExpr from the result of splitting a string representation of a racket program
+        on the '(', ')', and ' ' characters. """
     return racython.parser(string)
 
 
 def interp(expr, env=racython.getEnv()):
+    """ Rexp Environment -> Value Environment
+        Recursively evaluates the given Rexp in the given environment"""
     return racython.interp(expr, env)
 
 
 def run(string):
+    """ String -> String
+            Executes the given single function call racket program"""
     return str(racython.run(string))
 
 
